7f1021f8f85292838181b405d83907fa8b271f56
wondieki <wkerubo28@gmail.com>
2024-09-30 16:46:29 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    if (activity) {
+      return `This Saturday, I want to ${activity}!`;
+    } else {
+      return `This Saturday, I want to ${activity}!`;
+    }
+  }
+  function mondayWork(activity = "go to the office") {
+    if (activity) {
+      return `This Monday, I will ${activity}.`;
+    } else {
+      return `This Monday, I will ${activity}.`;
+    }
+  }
+  function wrapAdjective(highlight = "*") {
+    return function (adjective = "special") {
+      return `You are ${highlight}${adjective}${highlight}!`;
+    };
+  }
